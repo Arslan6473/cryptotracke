256a4c81cf55e9b45e7644cd879c980efaebc7e1
Arslan6473 <Arslansarwarwebdev@gmail.com>
2026-02-16 21:14:43 +0500

    latest

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,16 +1,21 @@
 import dbConnect from '@/lib/mongodb';
 import Blog from '@/models/Blog';
+import { NextResponse } from 'next/server';
 
-export default async function sitemap() {
+export const dynamic = 'force-dynamic';
+
+export async function GET() {
     await dbConnect();
     const blogs = await Blog.find({}).sort({ createdAt: -1 });
 
-    const blogEntries = blogs.map((blog) => ({
-        url: `https://yourdomain.com/blog/${blog.slug}`,
-        lastModified: new Date(blog.createdAt),
-        changeFrequency: 'weekly',
-        priority: 0.8,
-    }));
+    const blogEntries = blogs.map((blog) => `
+    <url>
+      <loc>https://yourdomain.com/blog/${blog.slug}</loc>
+      <lastmod>${new Date(blog.createdAt).toISOString()}</lastmod>
+      <changefreq>weekly</changefreq>
+      <priority>0.8</priority>
+    </url>
+  `);
 
     const staticPages = [
         {
@@ -45,5 +50,23 @@ export default async function sitemap() {
         },
     ];
 
-    return [...staticPages, ...blogEntries];
+    const staticXml = staticPages.map(page => `
+    <url>
+      <loc>${page.url}</loc>
+      <lastmod>${page.lastModified.toISOString()}</lastmod>
+      <changefreq>${page.changeFrequency}</changefreq>
+      <priority>${page.priority}</priority>
+    </url>
+  `);
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${[...staticXml, ...blogEntries].join('')}
+</urlset>`;
+
+    return new NextResponse(sitemap, {
+        headers: {
+            'Content-Type': 'application/xml',
+        },
+    });
 }
